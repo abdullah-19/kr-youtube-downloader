f62d7b2b4e0c3360d88b781369af98956f5320df
tulshidas <tulshidas37@gmail.com>
2019-02-23 04:37:44 +0600

    progress bar is making

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const ipcRenderer = require('electron').ipcRenderer;
 // wait for an updateReady message
 const downloadBtn = document.getElementById('downloadBtn');
@@ -7,6 +9,16 @@ const fileIcon = document.getElementById('fileIcon');
 const status_text = document.getElementById('error_text');
 const update_button = document.getElementById('updateBtn');
 const closeIcon = document.getElementById('closeIcon');
+const downloadingIcon = document.getElementById('downloadingIcon');
+
+
+function showThumbNailAndName(name,thumb_url) {
+  console.log("video name:" + name);
+  var thumbnail = document.getElementById("videoThumbnail");
+  //thumbnail.src = "downloads/thumbnail/" + name + ".jpg";
+  thumbnail.src = thumb_url;
+  thumbnail.style.display = "inline";
+}
 
 function fadeOut(id, val) {
   if (isNaN(val)) { val = 9; }
@@ -19,24 +31,23 @@ function fadeOut(id, val) {
   } else {
     document.getElementById(id).style.display = "none";
     return;
-   }
+  }
 }
 
-function fadeIn(id,val){
+function fadeIn(id, val) {
   // ID of the element to fade, Fade value[min value is 0]
-    if(isNaN(val)){ val = 0;}
-    document.getElementById(id).style.opacity='0.'+val;
-    //For IE
-    document.getElementById(id).style.filter='alpha(opacity='+val+'0)';
-    if(val<9){
-      val++;
-      setTimeout('fadeIn("'+id+'",'+val+')',200);
-    }else{return;}
-  }
-
+  if (isNaN(val)) { val = 0; }
+  document.getElementById(id).style.opacity = '0.' + val;
+  //For IE
+  document.getElementById(id).style.filter = 'alpha(opacity=' + val + '0)';
+  if (val < 9) {
+    val++;
+    setTimeout('fadeIn("' + id + '",' + val + ')', 200);
+  } else { return; }
+}
 
 function fadeOutUpdateBar() {
-  fadeOut("update-bar",9);
+  fadeOut("update-bar", 9);
 }
 
 function decreamentTransparency(element) {
@@ -53,7 +64,13 @@ update_button.addEventListener('click', function () {
 
 downloadBtn.addEventListener('click', function () {
   console.log('download button clicked');
+
+  downloadBtn.style.display = "none";
+  document.getElementById('video_div').style.display = "block";
+  processIcon.style.display = "inline-block";
   var url = urlField.value;
+
+  status_text.innerHTML = "Processing..";
   if (url == "") {
     status_text.innerHTML = "Please insert url";
   }
@@ -63,6 +80,8 @@ downloadBtn.addEventListener('click', function () {
   else {
     ipcRenderer.send('start_download', url);
   }
+
+  ipcRenderer.send('start_download', url);  
   console.log("Url:" + url);
 });
 
@@ -74,11 +93,99 @@ fileIcon.addEventListener('click', function () {
 
 ipcRenderer.on('download-started', function (event, arg) {
   // const message = `Message reply: ${arg}`
-  downloadBtn.style.display = "none";
-  processIcon.style.display = "inline-block";
+  //console.log(arg);
+  showThumbNailAndName(arg._filename,arg.thumbnails[0].url);
+  processIcon.style.display = "none";
+  downloadingIcon.style.display = "inline";
   status_text.innerHTML = "Downloading...";
   status_text.style.color = "blue";
+  //downloadProgress(arg._filename,arg.size);
+  showBasicInfo(arg);
+  downloadProgress(arg);
 });
+
+function showBasicInfo(info){
+    document.getElementById('filesize').innerHTML =  "Size(KB):"+info.size;
+    document.getElementById('duration').innerHTML = "Duration(h:m:s:):"+info._duration_hms;
+    document.getElementById('filename').innerHTML = "Filename:"+info._filename;
+}
+
+function showProgressBar(){
+  var progBar = document.getElementById('progress_bar');
+  progBar.style.display = "block";
+
+}
+
+function getFileSize(file) {
+  console.log('file to get size:' + file);
+  const stats = fs.statSync("downloads/" + file);
+  const fileSizeInBytes = stats.size;
+  //Convert the file size to megabytes (optional)
+  //const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+  return fileSizeInBytes;
+}
+
+function downloadProgress(info) {
+  var downloadedSize;
+  document.getElementById('progress_div').style.display = "block";
+  var progressStatus = document.getElementById('progress');
+ // var parentOfProgressBar = document.getElementById("parentOfprogressBar");
+  var progressBar = document.getElementById('progress_bar');
+  progressBar.style.display = "block";
+  var parcentOfProgress;
+  var parecentStr;
+  var progress = setInterval(() => {
+    downloadedSize = getFileSize(info._filename);
+    parcentOfProgress = downloadedSize/info.size;
+    parcentOfProgress = parcentOfProgress*100;
+    progressBar.style.width = parcentOfProgress+"%"; 
+    progressStatus.innerHTML = Math.floor(parcentOfProgress)+"%";
+    console.log(downloadedSize);
+    if (downloadedSize == info.size) {
+      ipcRenderer.send('download-complete');
+      parentOfProgressBar.style.display = "none";
+      clearInterval(progress);
+    }
+  }, 2000);
+}
+
+function move(oldPath, newPath, callback) {
+  console.log("new path:");
+  console.log(newPath);
+  var dir = newPath.substr(0, newPath.lastIndexOf(path.sep));
+  console.log("last index:"+newPath.lastIndexOf(path.sep));
+  console.log("new path:"+dir);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  fs.rename(oldPath, newPath, function (err) {
+    if (err) {
+      if (err.code === 'EXDEV') {
+        copy();
+      } else {
+        callback(err);
+      }
+      return;
+    }
+  });
+
+  function copy() {
+    var readStream = fs.createReadStream(oldPath);
+    var writeStream = fs.createWriteStream(newPath);
+
+    readStream.on('error', callback);
+    writeStream.on('error', callback);
+
+    readStream.on('close', function () {
+      fs.unlink(oldPath, callback);
+    });
+
+    readStream.pipe(writeStream);
+  }
+}
+
+
 
 ipcRenderer.on('download_error', function () {
 
@@ -92,24 +199,34 @@ ipcRenderer.on('download_error', function () {
 ipcRenderer.on('already_downloaded', function () {
   status_text.innerHTML = "File already exist";
   status_text.style.color = "green";
-  processIcon.style.display = "none";
+  downloadingIcon.style.display = "none";
   downloadBtn.style.display = "inline-block";
 });
 
 ipcRenderer.on('download-complete', function () {
   console.log("download completed message come:");
-  processIcon.style.display = "none";
+  downloadingIcon.style.display = "none";
   fileIcon.style.display = "inline-block";
   status_text.innerHTML = "Download completed";
   status_text.style.color = "green";
 });
 
+ipcRenderer.on('downloaded-thumbnail', function (event, thumbnailName) {
+  console.log("downloaded thumbnail from index.js file:");
+  showThumbNailAndName(thumbnailName);
+  ipcRenderer.send('download_video');
+});
+//filesize,ext,_duration_hms,_filename,size,fulltitle
 function clear_status() {
   status_text.innerHTML = "";
   if (downloadBtn.style.display == "none") {
     downloadBtn.style.display = "inline-block";
     processIcon.style.display = "none";
     fileIcon.style.display = "none";
+    var thumbnail = document.getElementById("videoThumbnail");
+    thumbnail.style.display = "none";
+    var videoName = document.getElementById("videoName");
+    videoName.style.display = "none";
   }
 }
 
@@ -125,5 +242,5 @@ function isValidUrl(url) {
 ipcRenderer.on('update_downloaded', function () {
   console.log("update-downloaded");
   document.getElementById("update-bar").style.display = "block";
-  fadeIn("update-bar",0);
+  fadeIn("update-bar", 0);
 });
--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-const {app, BrowserWindow, ipcMain,shell} = require('electron');
-const {autoUpdater} = require("electron-updater");
+const { app, BrowserWindow, ipcMain, shell } = require('electron');
+const { autoUpdater } = require("electron-updater");
 const path = require('path')
 const url = require('url')
 const exec = require('child_process').exec;
+var fetchVideoInfo = require('youtube-info');
+var fs = require('fs');
+var youtubedl = require('youtube-dl');
+//require('electron-reload')(__dirname);
 
+
+var videoInfo;
 let win;
 let outputLines;
-var download_path = path.join("\""+app.getPath('videos')+"\"","myDownloader","%(title)s.%(ext)s");
+var video_url;
+var download_path = path.join("\"" + app.getPath('videos') + "\"", "myDownloader", "%(title)s.%(ext)s");
 var destination_path;
 var downloadFileName;
-function createWindow () {
+var isWindowClosed = false;
+function createWindow() {
 
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({ width: 800, height: 600 })
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -19,19 +27,21 @@ function createWindow () {
     slashes: true
   }));
 
-
   win.on('closed', () => {
     win = null
   });
+  console.log('window created');
 }
 
-app.on('ready',()=>{
+app.on('ready', () => {
   createWindow();
   checkUpdate();
 });
 
 app.on('window-all-closed', () => {
+
   if (process.platform !== 'darwin') {
+    if (win === null) console.log("win value = null");
     app.quit();
   }
 });
@@ -42,42 +52,168 @@ app.on('activate', () => {
   }
 });
 
-ipcMain.on('start_download', function (event, url) {
-  console.log("in ipcMain:"+url);
-  var command = preperCommand(url);
-  execute(command, (output,error) => {
-      if(error != null){
-        event.sender.send('download_error');
-      }
-      else{
-        console.log(output);
-        if(output.indexOf("Destination:") != -1){
-          destination_path = output.split("Destination:")[1].split("\n")[0];
-          var splitted_words = destination_path.split(path.sep);
-          downloadFileName = splitted_words[splitted_words.length-1];
-          console.log("fileName:"+downloadFileName);
-          console.log("destination folder:"+destination_path);
-          event.sender.send('download-complete');
-        }
-        else if(output.indexOf("has already been downloaded") != -1){
-          event.sender.send('already_downloaded');
-        }
-        
-      }
-      
-  });
-  event.sender.send('download-started', 'Download not completed');
+ipcMain.on('start_download', function (event, arg) {
+  video_url = arg;
+  downloadUsingYDL();
 })
 
-ipcMain.on('open_file_directory', function(){
-  var downloadedFile_path = path.join(app.getPath('videos'),"myDownloader",downloadFileName);
-  console.log("downloaded file path:"+downloadedFile_path);
+function downloadUsingYDL() {
+  var filename;
+
+  var video = youtubedl('https://www.youtube.com/watch?v=LIJAsKCLTqc',
+    ['--format=18'],//"%(title)s.%(ext)s"
+    { cwd: __dirname });
+
+  video.on('info', function (info) {
+    videoInfo = info;
+    console.log(info);
+    console.log('thumbnail url:'+info.thumbnails[0].url);
+    filename = info._filename;
+    video.pipe(fs.createWriteStream("downloads/" + filename));
+    win.webContents.send('download-started',info);
+  });
+
+
+}
+
+function getFileSize(file) {
+  console.log('file to get size:' + file);
+  const stats = fs.statSync("downloads/" + file);
+  const fileSizeInBytes = stats.size;
+  //Convert the file size to megabytes (optional)
+  //const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+  return fileSizeInBytes;
+}
+
+function downloadProgress(fileName, totalSize) {
+  var downloadedSize;
+  var progress = setInterval(() => {
+    downloadedSize = getFileSize(fileName);
+    console.log(downloadedSize);
+    if (downloadedSize == totalSize) {
+      move("downloads/" + fileName, path.join(app.getPath('videos'), "kr_youtube_downloader", fileName), (err) => {
+        console.log(err);
+      });
+      clearInterval(progress);
+    }
+  }, 2000);
+}
+
+function move(oldPath, newPath, callback) {
+  console.log("new path:");
+  console.log(newPath);
+  var dir = newPath.substr(0, newPath.lastIndexOf(path.sep));
+  console.log("last index:"+newPath.lastIndexOf(path.sep));
+  console.log("new path:"+dir);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  fs.rename(oldPath, newPath, function (err) {
+    if (err) {
+      if (err.code === 'EXDEV') {
+        copy();
+      } else {
+        callback(err);
+      }
+      return;
+    }
+  });
+
+  function copy() {
+    var readStream = fs.createReadStream(oldPath);
+    var writeStream = fs.createWriteStream(newPath);
+
+    readStream.on('error', callback);
+    writeStream.on('error', callback);
+
+    readStream.on('close', function () {
+      fs.unlink(oldPath, callback);
+    });
+    readStream.pipe(writeStream);
+  }
+}
+
+ipcMain.on('download-complete', ()=>{
+  move("downloads/" + videoInfo._filename, path.join(app.getPath('videos'), "kr_youtube_downloader", videoInfo._filename), (err) => {
+    console.log(err);
+  });
+});
+
+function getVideoInfo() {
+  fetchVideoInfo("QohH89Eu5iM", function (err, videoInfo) {
+    if (err) throw new Error(err);
+    console.log(videoInfo);
+  });
+}
+
+ipcMain.on('download_video', function (event) {
+  download_video(event, video_url);
+})
+
+function download_thumbnail(event, url) {
+  var command = preperCommandForThumbnail(url);
+  execute(command, (output, error) => {
+    if (error != null) {
+      console.log('thumbnail eror');
+      event.sender.send('download_error');
+    }
+
+    else {
+      console.log('thumbnail download success');
+      console.log(output);
+      var thumbnail_destination = output.split("Writing thumbnail to")[1].split("\n")[0];
+      var thumbailDestinationWithoutExtension = thumbnail_destination.substr(0, thumbnail_destination.lastIndexOf('.'));
+      var thumbnailName = thumbailDestinationWithoutExtension.substr(thumbailDestinationWithoutExtension.lastIndexOf
+        (path.sep) + 1, thumbailDestinationWithoutExtension.length - 1);
+
+      console.log('thumbnail destination:' + thumbnail_destination);
+      console.log('thumbnail name:' + thumbnailName);
+      event.sender.send('downloaded-thumbnail', thumbnailName);
+
+    }
+
+  });
+}
+
+function download_video(event) {
+
+  console.log("in ipcMain:" + video_url);
+  var command = preperCommand(video_url);
+  execute(command, (output, error) => {
+    if (error != null) {
+      event.sender.send('download_error');
+    }
+    else {
+      console.log(output);
+      if (output.indexOf("Destination:") != -1) {
+        destination_path = output.split("Destination:")[1].split("\n")[0];
+        var splitted_words = destination_path.split(path.sep);
+        downloadFileName = splitted_words[splitted_words.length - 1];
+        console.log("fileName:" + downloadFileName);
+        console.log("destination folder:" + destination_path);
+        event.sender.send('download-complete');
+      }
+      else if (output.indexOf("has already been downloaded") != -1) {
+        event.sender.send('already_downloaded');
+      }
+
+    }
+
+  });
+  event.sender.send('download-started', 'Download not completed');
+
+}
+
+ipcMain.on('open_file_directory', function () {
+  var downloadedFile_path = path.join(app.getPath('videos'), "myDownloader", downloadFileName);
+  console.log("downloaded file path:" + downloadedFile_path);
   //shell.showItemInFolder(app.getPath('videos')+"/myDownloader/"+"\""+downloadFileName+"\"");
   shell.showItemInFolder(downloadedFile_path);
 })
 
-function showStatus(output){
-  setInterval(function(){
+function showStatus(output) {
+  setInterval(function () {
     console.log(output);
   }, 1000);
 }
@@ -90,28 +226,47 @@ function showStatus(output){
 //     });
 // });
 
-function preperCommand(url){
-  var plugin_path = path.join("\""+__dirname+"\"","downloads","youtube-dl");
+function preperCommand(url) {
+  var plugin_path = path.join("\"" + __dirname + "\"", "downloads", "youtube-dl");
   var command;
-  
-  console.log("download_path:"+download_path);
 
-  if(url.indexOf("playlist?list=") != -1) {
-    command = plugin_path + " -i -f mp4 --yes-playlist -o "+download_path+" "+url;
+  console.log("download_path:" + download_path);
+
+  if (url.indexOf("playlist?list=") != -1) {
+    command = plugin_path + " -i -f mp4 --yes-playlist -o " + download_path + " " + url;
     return command;
   }
-  else{
-      console.log("splitted Link:"+url.split("&")[0]);
-      command = plugin_path+" -o "+download_path+" "+url.split("&")[0];
-      console.log("command:"+command);
-      return command;
+  else {
+    console.log("splitted Link:" + url.split("&")[0]);
+    command = plugin_path + " -o " + download_path + " " + url.split("&")[0];
+    console.log("command:" + command);
+    return command;
+  }
+}
+
+function preperCommandForThumbnail(url) {
+  var plugin_path = path.join("\"" + __dirname + "\"", "downloads", "youtube-dl");
+  var thumbnail_path = path.join("\"" + __dirname + "\"", "downloads", "thumbnail", "%(title)s");
+  var command;
+
+  //console.log("download_path:" + download_path);
+
+  if (url.indexOf("playlist?list=") != -1) {
+    command = plugin_path + " -i -f mp4 --yes-playlist -o " + download_path + " " + url;
+    return command;
+  }
+  else {
+    console.log("splitted Link:" + url.split("&")[0]);
+    command = plugin_path + " --skip-download --write-thumbnail" + " -o " + thumbnail_path + " " + url.split("&")[0];
+    console.log("thumbnail command:" + command);
+    return command;
   }
 }
 
 function execute(command, callback) {
-    exec(command, (error, stdout, stderr) => { 
-        callback(stdout,error); 
-    });
+  exec(command, (error, stdout, stderr) => {
+    callback(stdout, error);
+  });
 };
 
 autoUpdater.on('update-downloaded', (info) => {
@@ -123,6 +278,11 @@ ipcMain.on("install-update", () => {
   autoUpdater.quitAndInstall();
 });
 
-function checkUpdate(){
-  autoUpdater.checkForUpdates();
+function checkUpdate() {
+  try {
+    autoUpdater.checkForUpdates();
+  } catch (err) {
+    console.log('some error in update checking');
+  }
+
 }
